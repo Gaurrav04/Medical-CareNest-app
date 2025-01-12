c6d2e1f3b86ec019cfa459a461c02db722a65a13
Gaurrav04 <94034679gd@gmail.com>
2025-01-12 23:13:24 +0530

    Initial commit

--- components/Frontend/Hero.tsx
+++ components/Frontend/Hero.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TransitionalText from "./TransitionalText"
 import SearchBar from "./SearchBar";
+import { Pill } from "lucide-react";
 
 const Hero = () => {
   const TEXTS = ['Ophthalmologists','Cardiologists','Endocrinologists',
@@ -31,7 +32,7 @@ const Hero = () => {
                       href="/#"
                       className="inline-flex items-center justify-center rounded-md bg-blue-600 px-6 py-3 text-center text-base font-medium text-white hover:bg-blue-700 lg:px-7"
                     >
-                      Get Started
+                      Need Doctor Urgently
                     </a>
                   </li>
                   <li>
@@ -40,35 +41,9 @@ const Hero = () => {
                       className="inline-flex items-center justify-center px-5 py-3 text-center text-base font-medium text-grey-800 hover:text-primary dark:text-white "
                       >
                       <span className="mr-2">
-                        <svg
-                          width="24"
-                          height="25"
-                          viewBox="0 0 24 25"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <circle cx="12" cy="12.6152" r="12" fill="#3758F9" />
-                          <rect
-                            x="7.99893"
-                            y="14.979"
-                            width="8.18182"
-                            height="1.63636"
-                            fill="white"
-                          />
-                          <rect
-                            x="11.2717"
-                            y="7.61523"
-                            width="1.63636"
-                            height="4.09091"
-                            fill="white"
-                          />
-                          <path
-                            d="M12.0898 14.1606L14.9241 11.0925H9.25557L12.0898 14.1606Z"
-                            fill="white"
-                          />
-                        </svg>
+                        <Pill className="flex-shrink-0 h-5 w-5 text-grey-700" />
                       </span>
-                      Download App
+                      Need a Refill
                     </a>
                   </li>
                 </ul>
--- components/Frontend/Navbar.tsx
+++ components/Frontend/Navbar.tsx
@@ -29,8 +29,8 @@ export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-stone-100">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
+    <header className="bg-stone-100 border-gray-800/10">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8 border-b ">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
