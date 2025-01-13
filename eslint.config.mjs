51e3f48cbd5c53a99523e1737976ca63268fd2a2
Gaurrav04 <94034679gd@gmail.com>
2025-01-13 11:15:08 +0530

    Initial committed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,8 +10,7 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.extends("next"), // Add 'next' configuration
+  ...compat.extends("next"),
   {
     rules: {
       'react/no-unescaped-entities': 'off',
