8b79dfa262d44cf1bc2033badfecec894d6e7bf2
Gaurrav04 <94034679gd@gmail.com>
2025-01-22 09:44:21 +0530

    added authentication

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({children}: {children:ReactNode}) {
         {/* <div className="bg-white mx-auto py-4 fixed top-20 w-full left-0 z-50 right-0 border-t border-gray-800/10 container">
         <MegaMenu />
         </div> */}
-        <div className="mt-[80px]">
+        <div className="mt-[60px]">
         {children}
         </div>
     </div>
