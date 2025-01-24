7759f294d3e2ddf7396daac4d8a40f83afe8033e
Gaurrav04 <94034679gd@gmail.com>
2025-01-24 17:47:36 +0530

    Doctor Page Check

--- app/(front)/join/doctors/page.tsx
+++ app/(front)/join/doctors/page.tsx
@@ -1,18 +1,24 @@
 import CustomButton from '@/components/CustomButton'
+import { Check } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
+    const features = [
+        "CareNest brings patients to you",
+        "Seamless e-processing experienced",
+        "Integrated clinical note-taking",
+    ]
   return (
     <div className="min-h-screen">
         <section className="py-12 px-4">
             <div className="max-w-6xl gap-4 mx-auto grid grid-cols-1 md:grid-cols-2">
           <div className="">
-          <h2 className="md:text-[3.2rem] text-[1.5rem] leading-[3.5rem]">
+          <h2 className="sm:text-[3.2rem] text-[1.5rem] leading-[3.5rem]">
             Build a successful {" "}
              <span className="text-blue-600 font-semibold">direct-pay</span>{" "}
-             practice with CareNest. 
+             practice with <span className="text-gray-600">CareNest.</span> 
             </h2>
             <p className="py-4">
             Welcome to CareNest - Empowering Your Practice, Enhancing Patient Care
@@ -20,7 +26,23 @@ export default function page() {
             That's why we've created a platform tailored to help you thrive, 
             whether you're just starting out or looking to expand your services.
             </p>
-            <CustomButton title="List your Service" href="#" className="bg-blue-600 hover:bg-gray-300"/>
+            <CustomButton 
+            title="List your Service"
+            href="#"
+            className="bg-blue-600 hover:bg-gray-300"
+            />
+            <div className="py-6">
+            {
+                features.map((feature,i) => {
+                    return (
+                        <p className="flex items-center">
+                            <Check className="w-4 h-4 mr-2 flex-shrink-0 text-blue-600"/>
+                            {feature}
+                        </p>
+                    )
+                })
+            }
+            </div>
           </div>
           <Image src="/Doctors.jpg" alt="" width={"1170"} height={848} className="w-full"/>
             </div>
