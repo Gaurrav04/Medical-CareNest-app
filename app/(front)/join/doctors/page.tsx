9737aaa886715371c16711d0deec36b77b4c09e3
Gaurrav04 <94034679gd@gmail.com>
2025-01-26 12:22:11 +0530

    Doctor Page

--- app/(front)/join/doctors/page.tsx
+++ app/(front)/join/doctors/page.tsx
@@ -1,15 +1,47 @@
 import CustomButton from '@/components/CustomButton'
+import CustomAccordion from '@/components/Frontend/CustomAccordion'
 import { Check } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
-    const features = [
-        "CareNest brings patients to you",
-        "Seamless e-processing experienced",
-        "Integrated clinical note-taking",
-    ]
+  const features = [
+    "CareNest brings patients to you",
+    "CareNest e-processing experienced",
+    "Integrated clinical note-taking",
+];
+const steps = [
+  "List your practice",
+  "Create competitive offerings",
+  "Start seeing patients",
+];
+const cards = [
+  {
+    title: "Begin Your Journey",
+    description:"Start a new application to join our network of healthcare providers.",
+    link: "/",
+    linkTitle:"Start a new application"
+  },
+  {
+    title: "Resume Application",
+    description:"Pick up where you left off and complete your onboarding process.Schedule for physical Approval",
+    link: "/",
+    linkTitle:"Continue your Application"
+  },
+  {
+    title: "Schedule a Call",
+    description:"Arrange a time for a call to finalize your application",
+    link: "/",
+    linkTitle:"Schedule a Call"
+  },
+  {
+    title: "Track your Progress",
+    description:"Monitor the status of your application and approvals in real-time.",
+    link: "/",
+    linkTitle:"Check Status"
+  },
+];
   return (
     <div className="min-h-screen">
         <section className="py-12 px-4">
@@ -47,6 +79,63 @@ export default function page() {
           <Image src="/Doctors.jpg" alt="" width={"1170"} height={848} className="w-full"/>
             </div>
         </section>
+        <section className="py-12 px-4">
+            <div className="max-w-6xl gap-8 mx-auto grid grid-cols-1 md:grid-cols-2">
+            <Image 
+             src="/Doctors.jpg" 
+             alt=""
+             width={"1170"} 
+             height={848} 
+             className="w-full hidden md:block mr-4"
+             />
+
+          <div className="">
+          <h2 className="sm:text-4xl text-2xl">
+            Join CareNest to increase your{" "}
+             <span className="text-blue-600 font-semibold">revenue</span>{" "}
+             today.
+            </h2>
+            <div className="py-6">
+            {
+                steps.map((feature,i) => {
+                    return (
+                        <p className="flex items-center">
+                            <Check className="w-4 h-4 mr-2 flex-shrink-0 text-blue-600"/>
+                            {feature}
+                        </p>
+                    );
+                })
+            }
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+             {
+              cards.map((card,i)=>{
+                return (
+                  <div key={i} className="bg-blue-500 p-4 rounded-lg shadow-2xl text-center">
+                  <h3 className="text-2xl font-semibold text-white">
+                    {card.title}
+                  </h3>
+                  <p className="text-gray-950 text-xs py-3">
+                    {card.description}
+                  </p>
+                  <CustomButton 
+                  title={card.linkTitle}
+                  href={card.link}
+                  className="bg-blue-700 hover:bg-cyan-500"
+                 />
+                </div>
+                )
+              })
+             }
+            </div>
+          </div>
+            </div>
+        </section>
+        <section className="py-12 px-4">
+            <div className="max-w-2xl gap-4 mx-auto">
+            <CustomAccordion/>
+          </div>
+        </section>
     </div>
-  )
+  );
 }
