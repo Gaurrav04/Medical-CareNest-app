355fc22d6939d52188c58b37c8ae50ccfee34a6b
Gaurrav04 <94034679gd@gmail.com>
2025-01-26 18:23:05 +0530

    Doctor page and Hospital Plans

--- app/(front)/joins/hospital/page.tsx
+++ app/(front)/joins/hospital/page.tsx
@@ -17,32 +17,7 @@ const steps = [
   "Create competitive offerings",
   "Start seeing patients",
 ];
-const cards = [
-  {
-    title: "Begin Your Journey",
-    description:"Start a new application to join our network of healthcare providers.",
-    link: "/",
-    linkTitle:"Start a new application"
-  },
-  {
-    title: "Resume Application",
-    description:"Pick up where you left off and complete your onboarding process.Schedule for physical Approval",
-    link: "/",
-    linkTitle:"Continue your Application"
-  },
-  {
-    title: "Schedule a Call",
-    description:"Arrange a time for a call to finalize your application",
-    link: "/",
-    linkTitle:"Schedule a Call"
-  },
-  {
-    title: "Track your Progress",
-    description:"Monitor the status of your application and approvals in real-time.",
-    link: "/",
-    linkTitle:"Check Status"
-  },
-];
+
 const faqs: FAQItem[] = [
 {
   qn: "How do I get started with CareNest?",
@@ -115,47 +90,7 @@ const faqs: FAQItem[] = [
           </div>
           <Image src="/Doctors.jpg" alt="" width={"1170"} height={848} className="w-full"/>
             </div>
-        </section>
-        <section className="py-20 px-4">
-            <div className="max-w-6xl gap-8 mx-auto grid grid-cols-1 md:grid-cols-2">
-            <Image 
-             src="/Doctors.jpg" 
-             alt=""
-             width={"1170"} 
-             height={848} 
-             className="w-full hidden md:block mr-4"
-             />
-
-          <div className="">
-          <h2 className="sm:text-3xl text-2xl">
-            Join CareNest to increase your{" "}
-             <span className="text-blue-600 font-semibold">revenue</span>{" "}
-             today.
-            </h2>
-            <div className="grid grid-cols-2 gap-4 py-6">
-             {
-              cards.map((card,i)=>{
-                return (
-                  <div key={i} className="bg-blue-500 p-4 rounded-lg shadow-2xl text-center">
-                  <h3 className="text-2xl font-semibold text-white">
-                    {card.title}
-                  </h3>
-                  <p className="text-gray-950 text-xs py-3">
-                    {card.description}
-                  </p>
-                  <CustomButton 
-                  title={card.linkTitle}
-                  href={card.link}
-                  className="bg-blue-700 hover:bg-cyan-500"
-                 />
-                </div>
-                )
-              })
-             }
-            </div>
-          </div>
-            </div>
-        </section>   
+        </section>  
         <section className="py-12 px-4">
             <div className="max-w-6xl gap-4 mx-auto">
             <Pricing />
