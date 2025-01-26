c1414da76d7797e9f78a65f522754b86527a7a6b
Gaurrav04 <94034679gd@gmail.com>
2025-01-26 21:24:18 +0530

    Dashboard

--- app/(back)/layout.tsx
+++ app/(back)/layout.tsx
@@ -5,13 +5,7 @@ import React, { ReactNode } from 'react'
 export default function Layout({children}: {children:ReactNode}) {
   return (
     <div>
-      <Navbar/>
-     <div className="flex">
-      <Sidebar/>
-       <div className="p-8">
-        {children}
-      </div>
+       <div className="">{children}</div>
      </div>
-    </div>
   )
 }
