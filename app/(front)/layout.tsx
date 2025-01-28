d8d50ef6d14fa839da1169822b91d5806209fb4f
Gaurrav04 <94034679gd@gmail.com>
2025-01-28 09:25:20 +0530

    Dashboard Login Dark Mode Support

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -4,11 +4,14 @@ import MegaMenu from "@/components/Frontend/MegaMenu";
 import React, { ReactNode } from 'react'
 import Footer from '@/components/Frontend/Footer';
 import { SiteHeader } from '@/components/site-header';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
-export default function Layout({children}: {children:ReactNode}) {
+export default async function Layout({children}: {children:ReactNode}) {
+  const session = await getServerSession(authOptions);
   return (
     <div className="">
-        <SiteHeader/>
+        <SiteHeader session={session}/>
         {children}
         <Footer/>
     </div>
